afbea52334c8b4ffa618ce2a5eec311fc744c84b
thuwsj <thuwsj@foxmail.com>
2025-10-29 15:23:13 +0900

    Add files via upload
    Comparative test: rtdetr

---
+++ c_rtdetr/1_train_detr.py
@@ -0,0 +1,41 @@
+# -*- coding:utf-8 -*-
+from ultralytics import RTDETR
+import argparse
+
+# 解析命令行参数
+parser = argparse.ArgumentParser(description='Train or validate YOLO model.')
+# train用于训练原始模型  val 用于得到精度指标
+parser.add_argument('--mode', type=str, default='train', help='Mode of operation.')
+
+# 训练时此处填写yaml文件路径 推理时此处填写网络结构.yaml文件路径
+parser.add_argument('--weights', type=str, default=r'rtdetr-baseline.yaml', help='Path to model file.')
+# 数据集存放路径
+parser.add_argument('--data', type=str, default='data.yaml', help='Path to data file.')
+
+parser.add_argument('--epoch', type=int, default=100, help='Number of epochs.')
+parser.add_argument('--batch', type=int, default=32, help='Batch size.')
+parser.add_argument('--workers', type=int, default=8, help='Number of workers.')
+parser.add_argument('--device', type=str, default='0', help='Device to use.')
+parser.add_argument('--name', type=str, default='rtdetr', help='Name data file.')
+parser.add_argument('--optimizer', type=str, default='SGD', help='optimizer.')
+parser.add_argument('--seed', type=str, default='0', help='seed.')
+parser.add_argument('--single_cls', type=bool, default=False, help='single_cls.')
+args = parser.parse_args()
+
+def train(model, data, epoch, batch, workers, device, name):
+    model.train(data=data, epochs=epoch, batch=batch, workers=workers, device=device, name=name,optimizer=args.optimizer,seed=int(args.seed),single_cls=args.single_cls)
+
+def validate(model, data, batch, workers, device, name):
+    model.val(data=data, batch=batch, workers=workers, device=device, name=name)
+
+
+def main():
+    model = RTDETR(args.weights)
+    if args.mode == 'train':
+        train(model, args.data, args.epoch, args.batch, args.workers, args.device, args.name)
+    else:
+        validate(model, args.data, args.batch, args.workers, args.device, args.name)
+
+
+if __name__ == '__main__':
+    main()
